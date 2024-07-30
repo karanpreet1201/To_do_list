62c74fa94989419bf60b0b352adb3b02fd3a22b9
Karanpreet Singh <151163925+karanpreet1201@users.noreply.github.com>
2024-07-30 21:30:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+import TaskField from "./components/TaskField";
+import TaskList from "./components/TaskList";
+import { TaskListContext } from "./components/TaskListContext";
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import NoteAltTwoToneIcon from "@mui/icons-material/NoteAltTwoTone";
+import Grid from "@mui/material/Grid";
+
+function App() {
+  const localTask = localStorage.getItem("taskList")
+    ? JSON.parse(localStorage.getItem("taskList"))
+    : [];
+  const [taskList, taskListUpdate] = useState(localTask);
+
+  return (
+    <Container>
+      <TaskListContext.Provider value={{ taskList, taskListUpdate }}>
+        <div className="card-holder">
+          <Card>
+            <CardContent>
+              <Grid container direction="row" alignItems="-">
+                <Grid item>
+                  <NoteAltTwoToneIcon color="primary" />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" component="div" color="primary">
+                    To Do List
+                  </Typography>
+                </Grid>
+              </Grid>
+              <TaskField />
+              <TaskList />
+            </CardContent>
+          </Card>
+        </div>
+      </TaskListContext.Provider>
+    </Container>
+  );
+}
+
+export default App;
